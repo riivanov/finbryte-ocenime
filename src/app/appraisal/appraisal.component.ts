2fc3e389dbe4952513fc58977ba16668b6bdcb9b
Radoslav Ivanov <riivanov63@gmail.com>
2024-01-18 17:27:12 +0200

    attempt to center map with flexbox; add checkboxes and submit button

--- src/app/appraisal/appraisal.component.ts
+++ src/app/appraisal/appraisal.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { AppraisalInputFormComponent } from '../appraisal-input-form/appraisal-input-form.component';
 
 @Component({
   selector: 'app-appraisal',
   standalone: true,
-  imports: [MatIconModule, AppraisalInputFormComponent],
+  imports: [
+    MatIconModule,
+    AppraisalInputFormComponent,
+    MatCheckboxModule,
+    MatButtonModule,
+  ],
   templateUrl: './appraisal.component.html',
   styleUrl: './appraisal.component.scss',
 })
